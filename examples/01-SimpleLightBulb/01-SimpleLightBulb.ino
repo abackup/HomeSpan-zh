fb905582757c3e0db1c2f1fbff6376c05ee97920
abackup <40161397+abackup@users.noreply.github.com>
2022-07-02 23:20:39 +0800

    Update 01-SimpleLightBulb.ino

--- examples/01-SimpleLightBulb/01-SimpleLightBulb.ino
+++ examples/01-SimpleLightBulb/01-SimpleLightBulb.ino
@@ -12,7 +12,7 @@ void setup() {
 //    new Characteristic::SerialNumber("123-ABC");    // 配件序列号（任意字符串，每个配件都可以相同）
 //    new Characteristic::Model("120-Volt Lamp");     // 配件型号（任意字符串，每个配件都可以相同）
 //    new Characteristic::FirmwareRevision("0.9");    // 配件的固件（任意文本字符串，每个配件都可以相同）
-    new Service::LightBulb();                       // 创建灯泡服务
+    new Service::LightBulb();                         // 创建灯泡服务
       new Characteristic::On();                       // 此服务需要“开”字符来打开和关闭灯
 
 } // end of setup()
