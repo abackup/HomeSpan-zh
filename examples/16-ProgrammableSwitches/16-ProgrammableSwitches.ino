b37d5ad1ab6bafd628e394e23b444e13c70d7264
abackup <40161397+abackup@users.noreply.github.com>
2022-07-15 12:40:47 +0800

    Update 16-ProgrammableSwitches.ino

--- examples/16-ProgrammableSwitches/16-ProgrammableSwitches.ino
+++ examples/16-ProgrammableSwitches/16-ProgrammableSwitches.ino
@@ -39,26 +39,20 @@
 
 void setup() {
 
-  // Example 16 does not introduce any new HomeSpan functionality, but instead showcases a unique feature of HomeKit that you can readily access with HomeSpan.
-  // In all prior examples we used the ESP32 to control a local appliance - something connected directly to the ESP32 device.  We've then seen how you can control
-  // the device via HomeKit's iOS or MacOS Home App, or by the addition of local pushbuttons connected directly to the ESP32 device.
-
-  // In this example we do the opposite, and use buttons on the ESP32 to control OTHER HomeKit devices.
+  // 示例 16 没有引入任何新的 HomeSpan 功能，而是展示了 HomeKit 的一个独特功能，您可以使用 HomeSpan 轻松访问该功能。在之前的
+// 所有示例中，我们使用 ESP32 来控制本地设备 - 直接连接到 ESP32 设备的设备。然后我们看到了如何通过 HomeKit 的 iOS 或 MacOS 
+//  Home App 或通过添加直接连接的本地按钮来控制设备到 ESP32 设备。
+// 在这个例子中，我们做相反的事情，并使用 ESP32 上的按钮来控制其他 HomeKit 设备。
+// 为此，我们使用 HomeKit 的 Stateless Programmable Switch Service。与其他只读服务（例如示例 12 中充分探讨的温度和空气质量传感器）
+ // 类似，Stateless Programmable Switch Service 仅侦听来自 HomeSpan 的事件通知，而不侦听来自 HomeSpan 的事件通知。尝试控制或更新
+ // HomeSpan 设备上的任何内容。更具体地说，无状态可编程开关服务侦听来自 HomeSpan 的 SINGLE、DOUBLE 或 LONG 按钮按下的通知。
+// 这些按钮按下的含义是不受 HomeSpan 控制的。相反，您可以直接在 Home 应用程序中对其操作进行编程。以这种方式，HomeSpan 成为通用按钮
+//  的平台，您可以通过编程来控制任何其他 HomeKit 配件，甚至触发 HomeKit 场景.
+// 运行此配置并与 HomeKit 配对后，您的 Home App 应该会显示一个标有“PushButton Switches”的新图块。单击该图块将打开一个新页面，您可
+ // 以在其中对每个按钮的操作进行编程。这些操作可以在无需修改 HomeSpan 代码，甚至重启设备。
   
-  // To do so, we use HomeKit's Stateless Programmable Switch Service.  Similar to other read-only Services, such as the Temperature and Air Quality Sensors 
-  // fully explored in Example 12, the Stateless Programmable Switch Service only listens for event notifications coming from HomeSpan and does not try to control
-  // or update anything on the HomeSpan Device.  More specifically, the Stateless Programmable Switch Service listens for notifications of a SINGLE, DOUBLE,
-  // or LONG button press coming from HomeSpan.
-  
-  // What these button presses mean is outside the control of HomeSpan.  Instead, you program their actions directly in the Home App.  In this fashion, HomeSpan
-  // becomes a platform for generic buttons that you can program to control any other HomeKit accessory or even trigger HomeKit scenes.
-
-  // Upon running this configuration and pairing to HomeKit, your Home App should reveal a new tile labeled "PushButton Switches."  Clicking that tile will open up
-  // a new page where you can program the actions of each of the buttons.  These actions can be changed at any time without any need to modify the HomeSpan code,
-  // or even reboot the device. 
-
-  // The code for this is quite simple, and as usual we've encapsulated all the functionality in a standalone file: DEV_ProgButton.h.  Below we create two generic
-  // buttons, one connected to pin 23, and one connected to pin 5.  See DEV_ProgButton.h for complete details. 
+ //代码非常简单，像往常一样，我们将所有功能封装在一个独立文件中：DEV_ProgButton.h。下面我们创建两个通用按钮，一个连接到引脚 23，一
+  //个连接到引脚 5。参见 DEV_ProgButton。 h 了解完整的详细信息。
   
   Serial.begin(115200);
 
@@ -73,13 +67,10 @@ void setup() {
       new Characteristic::Identify(); 
       new Characteristic::Name("PushButton Switches");
 
-    // We've written DEV_ProgButton to take two arguments.  The first is a pin number that DEV_ProgButton.h uses to create a SpanButton.  The second is an index number
-    // that HomeKit uses as a label when you program the actions of each button in the Home App.  The numbers do not have to be sequential, nor start with 1.  They just need
-    // to be unique so HomeKit can distinguish them.  Note that HomeKit does not require index numbers if you only have one Stateless Programmable Switch Service within any
-    // given Accessory.  Since we have two, we must specify two unique index numbers.
+    // // 我们编写了 DEV_ProgButton 来接受两个参数。 第一个是 DEV_ProgButton.h 用于创建 SpanButton 的 pin 号。 第二个是 HomeKit 在编写 Home App 中每个按钮的操作时用作标签的索引号。 这些数字不必是连续的，也不必以 1 开头。它们只需要是唯一的，以便 HomeKit 可以区分它们。 请注意，如果您在任何给定附件中只有一个无状态可编程开关服务，则 HomeKit 不需要索引号。 由于我们有两个，我们必须指定两个唯一的索引号。
     
-    new DEV_ProgButton(23,1);       // create Stateless Programmable Switch Service on pin 23 with index=1
-    new DEV_ProgButton(5,2);        // create Stateless Programmable Switch Service on pin 5 with index=2
+    new DEV_ProgButton(23,1);       // 在引脚 23 上创建无状态可编程开关服务，索引 = 1
+    new DEV_ProgButton(5,2);        // 在引脚 5 上创建无状态可编程开关服务，索引 = 2
  
 } // end of setup()
 
