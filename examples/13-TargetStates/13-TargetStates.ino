a9b1837c7b7f11aeee42760fe01cccae1ed91e1b
abackup <40161397+abackup@users.noreply.github.com>
2022-07-15 11:41:22 +0800

    Update 13-TargetStates.ino

--- examples/13-TargetStates/13-TargetStates.ino
+++ examples/13-TargetStates/13-TargetStates.ino
@@ -41,37 +41,23 @@
 
 void setup() {
 
-  // In Example 12 we saw how to implement the loop() method for a Service to continuously monitor our device and periodically report
-  // changes in one or more Characteristics back to HomeKit using setVal() and timeVal().  In that example we implemented passive sensors
-  // that operated independently and required no input from the user, which meant we did not need to implement any update() methods.
+  // 在示例 12 中，我们看到了如何为服务实现 loop() 方法，以持续监控我们的设备，并使用 setVal() 和 timeVal() 定期向 HomeKit 
+ //报告一个或多个特性的变化。在该示例中，我们实现了被动传感器独立操作，不需要用户输入，这意味着我们不需要实现任何 update() 方法。
+  
+ //在这个示例 13 中，我们通过实现两个新服务来演示同时使用 update() 和 loop() 方法：车库门开启器和电动窗帘。两个示例都展示了
+ // HomeKit 的目标状态/当前状态框架。对于需要时间操作的物理设备（例如关门），HomeKit 服务通常使用：
+//* HomeKit 通过对 HomeSpan 的 update() 请求设置的一个特性，它表示所需的目标状态，例如打开、关闭或在某些情况下打开或关闭的百分比；和
+//* HomeSpan 用于在 loop() 方法中跟踪设备的当前状态以及使用 setVal() 将更改报告回 HomeKit 的一个只读特性。
+// 并非所有 HomeKit 服务都使用相同的特性来定义目标和当前状态。一些服务使用特定于该服务的特性，而其他服务使用更通用的特性。共同的
+ // 主题似乎是 HomeKit 猜测设备正在执行的操作，并通过比较它设置的目标状态特性的值和它以事件通知的形式接收的当前状态特性的值来相应
+  //地更新它的平铺图标。当它们相同时，HomeKit 假定物理设备已到达所需的位置。当它们不同时，HomeKit 会假设某些东西会打开、关闭、
+  //升高、降低等。HAP 文档中概述了每个服务的此过程的详细信息，但请注意该文档并不总是与最新版本的最新版本保持同步。 HomeKit 应用
+ // 程序。有时需要进行一些试验和大量的反复试验才能完全理解每个服务如何响应不同的组合离子的特征值。
+// 与往常一样，我们不会将 ESP32 连接到实际的车库门或窗帘，而是会模拟它们的响应和动作以用于说明目的。在某些方面，由于需要模拟值，
+//  代码更复杂——如果我们实际上连接到车库门或窗帘，可能会更容易！
+// 我们的两个派生服务的完整注释代码可以在 DEV_DoorsWindows.h 中找到。这些示例没有引入任何新的 HomeSpan 函数或特性。而是我们将迄
+//  今为止学到的所有内容组合成两个相当复杂的服务。您可能想要参考 HAP这两个父服务的文档，以充分理解每个服务特征的不同值设置的含义。
 
-  // In this Example 13 we demonstrate the simultaneous use of both the update() and loop() methods by implementing two new Services:
-  // a Garage Door Opener and a motorized Window Shade.  Both examples showcase HomeKit's Target-State/Current-State framework.
-  // For physical devices that take time to operate (such as closing a door), HomeKit Services typically use:
-  
-  //    * one Characteristic that HomeKit sets via update() requests to HomeSpan, and that represent a desired target state,
-  //      such as opened, closed, or in some cases a percentage opened or closed; and
-  
-  //    * one read-only Characteristic that HomeSpan use to track the current state of the device in the loop() method, as well as
-  //      report back changes to HomeKit using setVal().
-  
-  // Not all HomeKit Services utilize the same Characteristics to define target and current states.  Some Services use Characteristics
-  // that are specific to that one Service, whereas others use more generic Characteristics.  The common theme seems to be that HomeKit
-  // guesses the actions a device is taking, and updates it tile's icon accordingly, by comparing the value of the target state
-  // Characteristic it sets, and the current state Characteristic it receives in the form of Event Notifications.  When they are the same,
-  // HomeKit assumes the physical device has reached the required position.  When they differ, HomeKit assumes something will be opening,
-  // closing, raising, lowering, etc.  The details of this process for each Service is outlined in the HAP documentation, but beware
-  // the document is not always up to date with the lastest version of the HomeKit application.  Sometimes a little experimenting and a lot
-  // of trial and error is required to fully understand how each Service responds to different combinations of Characteristic values.
-  
-  // As always, we won't be connecting our ESP32 to an actual garage door or window shade but will instead simulate their responses and
-  // actions for illustrative purposes. In some ways the code is more complicated because of the need to simulate values - it might be
-  // easier if we actually were connecting to a garage door or window shade!
-  
-  // Fully commented code for both of our derived Services can be found in DEV_DoorsWindows.h.  These examples do not introduce any new
-  // HomeSpan functions or features.  Rather we are combining everything learned so far into two reasonably complex Services.  You may
-  // want to reference the HAP documentation for these two parent Services to fully understand the meaning of the different value settings
-  // for each of the Services' Characteristics.
   
   Serial.begin(115200);
 
@@ -85,13 +71,13 @@ void setup() {
     new Service::AccessoryInformation();
       new Characteristic::Identify(); 
       new Characteristic::Name("Garage Door");
-    new DEV_GarageDoor();                                                               // Create a Garage Door Opener (see DEV_DoorsWindows.h for definition)
+    new DEV_GarageDoor();                                                               //创建车库开门器（参见 DEV_DoorsWindows.h 的定义）
 
   new SpanAccessory();                                                          
     new Service::AccessoryInformation();
       new Characteristic::Identify(); 
       new Characteristic::Name("Window Shade");
-    new DEV_WindowShade();                                                              // Create a motorized Window Shade (see DEV_DoorsWindows.h for definition)
+    new DEV_WindowShade();                                                              // 创建电动窗帘（参见 DEV_DoorsWindows.h 的定义）
 
 } // end of setup()
 
