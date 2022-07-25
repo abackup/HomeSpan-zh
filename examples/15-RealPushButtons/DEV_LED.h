1036f0303a6cfa39727c14df10ca5ce3d5377fd7
abackup <40161397+abackup@users.noreply.github.com>
2022-07-25 13:12:34 +0800

    Update DEV_LED.h

--- examples/15-RealPushButtons/DEV_LED.h
+++ examples/15-RealPushButtons/DEV_LED.h
@@ -3,11 +3,11 @@
 //   DEVICE-SPECIFIC LED SERVICES //
 ////////////////////////////////////
 
-#include "extras/PwmPin.h"                          // library of various PWM functions
+#include "extras/PwmPin.h"                          // 各种 PWM 函数库
 
 ////////////////////////////////////
 
-struct DEV_DimmableLED : Service::LightBulb {       // Dimmable LED
+struct DEV_DimmableLED : Service::LightBulb {       //可调光LED
 
   // 此版本的可调光 LED 服务类似于示例 11 中最后使用的版本，但现在包括对 3 个物理按钮的支持
    // 执行以下操作：
@@ -41,16 +41,16 @@ struct DEV_DimmableLED : Service::LightBulb {       // Dimmable LED
      // 增加/减少亮度，打开/关闭电源，设置/重置最喜欢的亮度级别的所有逻辑都找到了
      // 在下面的 button() 方法中。
 
-    new SpanButton(powerPin);                       // NEW! create new SpanButton to control power using pushbutton on pin number "powerPin"
-    new SpanButton(raisePin,500);                   // NEW! create new SpanButton to increase brightness using pushbutton on pin number "raisePin"
-    new SpanButton(lowerPin,500);                   // NEW! create new SpanButton to decrease brightness using pushbutton on pin number "lowerPin"
+    new SpanButton(powerPin);                       // 新的！ 使用引脚号“powerPin”上的按钮创建新的 SpanButton 以控制电源
+    new SpanButton(raisePin,500);                   // 新的！ 使用引脚号“raisePin”上的按钮创建新的 SpanButton 以增加亮度
+    new SpanButton(lowerPin,500);                   // 新的！ 使用引脚号“lowerPin”上的按钮创建新的 SpanButton 以降低亮度
 
-    this->powerPin=powerPin;                        // NEW! save power pushbutton pin number
-    this->raisePin=raisePin;                        // NEW! save increase brightness pushbutton pin number
-    this->lowerPin=lowerPin;                        // NEW! save decrease brightness pushbutton pin number
-    this->ledPin=new LedPin(pin);                   // configures a PWM LED for output to the specified pin
+    this->powerPin=powerPin;                        // 新的！ 保存电源按钮引脚号
+    this->raisePin=raisePin;                        // 新的！ 保存增加亮度按钮引脚号
+    this->lowerPin=lowerPin;                        // 新的！ 保存降低亮度按钮引脚号
+    this->ledPin=new LedPin(pin);                   // 将 PWM LED 配置为输出到指定引脚
 
-    Serial.print("Configuring Dimmable LED: Pin="); // initialization message
+    Serial.print("Configuring Dimmable LED: Pin="); // 初始化消息
     Serial.print(ledPin->getPin());
     Serial.print(" Channel=");
     Serial.print(channel);
@@ -85,11 +85,11 @@ struct DEV_DimmableLED : Service::LightBulb {       // Dimmable LED
   
   } // update
 
-  // NEW!  Here is the button() method where all the PushButton actions are defined.   Take note of the signature, and use of the word "override" 
+  // 新的！ 这是定义所有 PushButton 操作的 button() 方法。 记下签名，并使用“覆盖”一词
 
   void button(int pin, int pressType) override {
 
-    LOG1("Found button press on pin: ");            // always a good idea to log messages
+    LOG1("Found button press on pin: ");            // 记录消息总是一个好主意
     LOG1(pin);
     LOG1("  type: ");
     LOG1(pressType==SpanButton::LONG?"LONG":(pressType==SpanButton::SINGLE)?"SINGLE":"DOUBLE");
@@ -98,21 +98,21 @@ struct DEV_DimmableLED : Service::LightBulb {       // Dimmable LED
     int newLevel;
 
     if(pin==powerPin){
-      if(pressType==SpanButton::SINGLE){            // if a SINGLE press of the power button...
-        power->setVal(1-power->getVal());           // ...toggle the value of the power Characteristic
+      if(pressType==SpanButton::SINGLE){            // 如果单按电源按钮..
+        power->setVal(1-power->getVal());           // ...切换功率特性的值
       } else
       
-      if(pressType==SpanButton::DOUBLE){            // if a DOUBLE press of the power button...
-        power->setVal(1);                           // ...turn on power
-        level->setVal(favoriteLevel);               // ...and set brightness to the favorite level        
+      if(pressType==SpanButton::DOUBLE){            // 如果双击电源按钮...
+        power->setVal(1);                           // ...打开电源
+        level->setVal(favoriteLevel);               // ...并将亮度设置为最喜欢的级别
       } else
       
-      if(pressType==SpanButton::LONG) {             // if a LONG press of the power button...
-        favoriteLevel=level->getVal();              // ...save the current brightness level
-        LOG1("Saved new brightness level=");        // ...and output log message
+      if(pressType==SpanButton::LONG) {             // 如果长按电源按钮...
+        favoriteLevel=level->getVal();              // ...保存当前亮度级别
+        LOG1("Saved new brightness level=");        // ...并输出日志信息
         LOG1(favoriteLevel);
         LOG1("\n");        
-        ledPin->set((1-power->getVal())*level->getVal());       // blink the LED to indicate new level has been saved
+        ledPin->set((1-power->getVal())*level->getVal());       // 闪烁 LED 表示新级别已保存
         delay(100);
         ledPin->set((1-power->getVal())*level->getVal());
       }
@@ -147,38 +147,38 @@ struct DEV_DimmableLED : Service::LightBulb {       // Dimmable LED
 
     }
 
-    // 在示例 14 中，我们看到了如何在 HomeKit 中模拟 PushButton tile，方法是自动重置 Characteristic，使其在短时间内“关闭”。然而，
-   // 有时我们希望能够使用实际的PushButtons（或瞬时开关）物理控制设备) 触发动作，例如打开电灯或风扇，或打开车库门。此外，我们希望
-   //   HomeKit 反映此类手动操作导致的设备变化 - HomeKit 应该知道灯何时打开或手动关闭。
+//在示例 14 中，我们看到了如何在 HomeKit 中模拟 PushButton tile，方法是自动重置 Characteristic，使其在短时间内“关闭”。然而，
+//有时我们希望能够使用实际的PushButtons（或瞬时开关）物理控制设备) 触发动作，例如打开电灯或风扇，或打开车库门。此外，我们希望
+//HomeKit 反映此类手动操作导致的设备变化 - HomeKit 应该知道灯何时打开或手动关闭。
 //一种方法是通过添加到派生服务的 loop() 方法中的自定义代码来监视按钮，检查按钮何时被按下，消除按钮噪音，在按下时执行一些操作，并使
-  //    用 setVal( ） 方法。或者您可以使用 HomeSpan 的内置 SpanButton() 对象。
+//用 setVal( ） 方法。或者您可以使用 HomeSpan 的内置 SpanButton() 对象。
 //SpanButton() 是一个服务级对象，这意味着它将自己附加到您定义的最后一个服务。通常，您将直接在派生服务的构造函数中实例化多个
-  //             SpanButton() 对象。
+//SpanButton() 对象。
 //SpanButton() 支持三种类型的触发器：单次按钮按下、双次按下和长（扩展）按下。
 //触发这些不同类型所需的按下的长度可以由 SpanButton() 的可选参数指定。由于大多数按钮在按下时会产生虚假噪音（然后在释放时再次产生），
-      //         触发 SINGLE 按下的默认时间是 5ms。可以将其更改为更长的值，但不建议使用更短的值，因为这可能会允许虚假触发，除非您
-     //          使用硬件对开关进行去抖动。
+//触发 SINGLE 按下的默认时间是 5ms。可以将其更改为更长的值，但不建议使用更短的值，因为这可能会允许虚假触发，除非您
+//使用硬件对开关进行去抖动。
 //SpanButton() 构造函数接受 4 个参数，顺序如下：pin - PushButton 连接到的 pin 号（必需）longTime - 按钮需要按下的最小时间长度（以
-    //           毫秒为单位）才能被视为 LONG press (optional; default=2000 ms)singleTime- 最小时间长度（以毫秒为单位） 需要按下按
-    //           钮才能被视为 SINGLE press（可选；默认值=5 ms）doubleTime- 最大时间长度（以毫秒为单位） ) 在两次按下按钮之间创建两
-    //  次按下（可选；默认值 = 200 毫秒）
+//毫秒为单位）才能被视为 LONG press (optional; default=2000 ms)singleTime- 最小时间长度（以毫秒为单位） 需要按下按
+//钮才能被视为 SINGLE press（可选；默认值=5 ms）doubleTime- 最大时间长度（以毫秒为单位） ) 在两次按下按钮之间创建两
+//次按下（可选；默认值 = 200 毫秒）
 //当 SpanButton() 被实例化时，它将 ESP32 上的指定引脚设置为带 PULL-UP 的 INPUT，这意味着该引脚在读取时通常会返回 HIGH 值。应连接实
- //     际的 PushButton，以便该引脚接地按下按钮时。
+//际的 PushButton，以便该引脚接地按下按钮时。
 //HomeSpan 自动轮询所有具有关联 SpanButton() 对象的引脚并检查 LOW 值，这表明按钮被按下，但尚未释放。然后它启动一个计时器。如果按钮
-  //    在被按下少于 singleTime 毫秒后被释放，则没有任何反应.如果按钮被按下超过 singleTime 毫秒后释放，但小于 longTime 毫秒，则触
-  //    发 SINGLE 按下，除非您在 doubleTime 毫秒内再次按下以触发 DOUBLE 按下。如果按住按钮超过 longTime毫秒而不被释放，触发长按。
-  //    一旦触发长按，计时器就会重置，因此如果您按住按钮，另一个长按将在另一个 longTime 毫秒内触发。这将一直持续到您最终释放按钮。
+//在被按下少于 singleTime 毫秒后被释放，则没有任何反应.如果按钮被按下超过 singleTime 毫秒后释放，但小于 longTime 毫秒，则触
+//发 SINGLE 按下，除非您在 doubleTime 毫秒内再次按下以触发 DOUBLE 按下。如果按住按钮超过 longTime毫秒而不被释放，触发长按。
+//一旦触发长按，计时器就会重置，因此如果您按住按钮，另一个长按将在另一个 longTime 毫秒内触发。这将一直持续到您最终释放按钮。
 //请注意，如果您设置 longTime > singleTime，SpanButton() 将仅触发 LONG 按下。此外，如果您将 doubleTime 设置为零，SpanButton() 将
-   //   无法触发 DOUBLE 按下。
+// 无法触发 DOUBLE 按下。
 //要在派生服务中使用 SpanButton()，您需要实现 button() 方法。与 loop() 方法类似，您的 button() 方法通常包含 getVal() 函数和 
-  //    setVal() 函数的某种组合，以及在物理设备上执行一些操作的代码（将引脚设置为高或低，打开风扇等）。但是，与 HomeSpan 每个轮询
-  //    周期调用的 loop() 方法相比，HomeSpan 只调用 button()当附加到服务的按钮注册为单次、双次或长按时的方法。
+//setVal() 函数的某种组合，以及在物理设备上执行一些操作的代码（将引脚设置为高或低，打开风扇等）。但是，与 HomeSpan 每个轮询
+//周期调用的 loop() 方法相比，HomeSpan 只调用 button()当附加到服务的按钮注册为单次、双次或长按时的方法。
 //不要忘记为实际的 LED 设置新的功率和电平——上面的代码本身只会改变 HomeKit 中的特性值！我们仍然需要对实际的 LED 本身采取行动。
- //请注意，下面的行类似于上面 update() 方法中使用的 ledPin->set 函数，但并不相同。在 update() 方法中，我们使用 getNewVal() 是因为
-    //  我们想更改 LED 以匹配用户通过 HomeKit 控制器请求的新值。我们不需要（也不能）使用 setVal() 在 update() 方法中修改这些值，
-     // 因为 HomeSpan 会自动为我们执行此操作，前提是我们返回 StatusCode::OK在 update() 方法的末尾。
- //但是在 button() 方法中，getNewVal() 没有任何意义，因为 HomeKit 不会调用 button() 方法来响应来自 HomeKit Controller 接口的用户
-        //请求。相反，我们手动更改了一个或多个 Characteristic 的值使用 setVal() 响应 SINGLE、DOUB
+//请注意，下面的行类似于上面 update() 方法中使用的 ledPin->set 函数，但并不相同。在 update() 方法中，我们使用 getNewVal() 是因为
+//我们想更改 LED 以匹配用户通过 HomeKit 控制器请求的新值。我们不需要（也不能）使用 setVal() 在 update() 方法中修改这些值，
+//因为 HomeSpan 会自动为我们执行此操作，前提是我们返回 StatusCode::OK在 update() 方法的末尾。
+//但是在 button() 方法中，getNewVal() 没有任何意义，因为 HomeKit 不会调用 button() 方法来响应来自 HomeKit Controller 接口的用户
+//请求。相反，我们手动更改了一个或多个 Characteristic 的值使用 setVal() 响应 SINGLE、DOUB
     
     ledPin->set(power->getVal()*level->getVal());       // update the physical LED to reflect the new values
 
