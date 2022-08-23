7e663d03da947dc4da91e86a5ecd00aca5c2516f
abackup <40161397+abackup@users.noreply.github.com>
2022-08-23 16:56:19 +0800

    Update 20-AdvancedTechniques.ino

--- examples/20-AdvancedTechniques/20-AdvancedTechniques.ino
+++ examples/20-AdvancedTechniques/20-AdvancedTechniques.ino
@@ -40,25 +40,25 @@
 
 #include "HomeSpan.h"
 
-  // In Example 20 we will implement a bridge device supporting up to 10 Lightbulb Accessories.  However, rather than pre-specifying the number of Lights, we
-  // will allow Light Accessories to be added and deleted dynamically by the user via the CLI.  Changes are reflected in the Home App without the need to restart
-  // the device!  Note this example uses a variety of advanced HomeSpan functions, as well as some detailed features of both the ESP32-IDF and C++ that have not been used
-  // in any of the previous examples.
+    // 在示例 20 中，我们将实现一个桥接设备，最多支持 10 个灯泡配件。 但是，我们不是预先指定灯的数量，而是
+   // 将允许用户通过 CLI 动态添加和删除 Light Accessories。 更改会反映在 Home App 中，无需重新启动
+   // 设备！ 注意这个例子使用了各种高级 HomeSpan 函数，以及 ESP32-IDF 和 C++ 的一些详细特性，但没有使用
+   // 在前面的任何示例中。
 
-  // We will use a C++ array with 10 elements containing integers representing the Light "ID" of each Lightbulb Accessory implemented.  An ID of zero means there is no
-  // Light defined in that element.
+   // 我们将使用一个包含 10 个元素的 C++ 数组，其中包含表示实现的每个灯泡附件的灯“ID”的整数。 ID 为零表示没有
+   // 在该元素中定义的光。
   
-#include <array>                // include the C++ standard library array container
+#include <array>                // 包含 C++ 标准库数组容器
 
-std::array<int,10> lights;      // declare "lights" to be an array of 10 integers
+std::array<int,10> lights;      // 将“lights”声明为 10 个整数的数组
 
-using std::fill;                // place the std library function fill, remove, and find, into the global namespace so we can use them below without prefacing with "std::"
+using std::fill;                // 将 std 库函数 fill、remove 和 find 放入全局命名空间中，这样我们就可以在下面使用它们，而无需以“std::”开头
 using std::remove;
 using std::find;
 
-  // We will use non-volatile storage (NVS) to store the lights array so that the device can restore the current configuration upon rebooting
-  
-nvs_handle savedData;           // declare savdData as a handle to be used with the NVS (see the ESP32-IDF for details on how to use NVS storage)
+  // 我们将使用非易失性存储 (NVS) 来存储灯阵列，以便设备可以在重启时恢复当前配置
+
+nvs_handle savedData;           // 将 savdData 声明为要与 NVS 一起使用的句柄（有关如何使用 NVS 存储的详细信息，请参阅 ESP32-IDF）
 
 
 //////////////////////////////////////
@@ -67,26 +67,26 @@ void setup() {
 
   Serial.begin(115200);
 
-  fill(lights.begin(),lights.end(),0);                   // initialize lights array with zeros in each of the 10 elements (no Light Accessories defined)
+  fill(lights.begin(),lights.end(),0);                   // 用 10 个元素中的每个元素中的零初始化灯光数组（未定义灯光配件）
   
   size_t len;  
-  nvs_open("SAVED_DATA",NVS_READWRITE,&savedData);       // open a new namespace called SAVED_DATA in the NVS
-  if(!nvs_get_blob(savedData,"LIGHTS",NULL,&len))        // if LIGHTS data found
-    nvs_get_blob(savedData,"LIGHTS",&lights,&len);       // retrieve data
+  nvs_open("SAVED_DATA",NVS_READWRITE,&savedData);       // 在 NVS 中打开一个名为 SAVED_DATA 的新命名空间
+  if(!nvs_get_blob(savedData,"LIGHTS",NULL,&len))        // 如果找到 LIGHTS 数据
+    nvs_get_blob(savedData,"LIGHTS",&lights,&len);       // 检索数据
 
   homeSpan.setLogLevel(1);
 
   homeSpan.begin(Category::Lighting,"HomeSpan Lights");
 
-  // We begin by creating the Bridge Accessory
+  // 我们首先创建桥接配件
 
-  new SpanAccessory(1);                                   // here we specified the AID=1 for clarity (it would default to 1 anyway if left blank)
+  new SpanAccessory(1);                                   // 为了清楚起见，我们在这里指定了 AID=1（如果留空，它将默认为 1）
     new Service::AccessoryInformation();
       new Characteristic::Identify();
-      new Characteristic::Model("HomeSpan Dynamic Bridge");     // defining the Model is optional
+      new Characteristic::Model("HomeSpan Dynamic Bridge");     // 定义模型是可选的
 
-  // Now we create Light Accessories based on what is recorded in the lights array
-  // We'll use C++ iterators to loop over all elements until we reach the end of the array, or find an element with a value of zero
+  // 现在我们根据灯光阵列中记录的内容创建灯光配件
+  // 我们将使用 C++ 迭代器遍历所有元素，直到到达数组的末尾，或者找到一个值为零的元素
 
   for(auto it=lights.begin(); it!=lights.end() && *it!=0; it++)       // loop over all elements (stopping when we get to the end, or hit an element with a value of zero)
     addLight(*it);                                                    // call addLight (defined further below) with an argument equal to the integer stored in that element
