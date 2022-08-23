4a1f99ea7f8a8dece4c8648a4329b88d315ce603
abackup <40161397+abackup@users.noreply.github.com>
2022-08-23 16:27:18 +0800

    Update 15-RealPushButtons.ino

--- examples/15-RealPushButtons/15-RealPushButtons.ino
+++ examples/15-RealPushButtons/15-RealPushButtons.ino
@@ -41,50 +41,93 @@
 
 void setup() {
 
-  // 在示例 14 中，我们看到了如何在 HomeKit 中模拟 PushButton tile，方法是自动重置 Characteristic，使其在短时间内“关闭”。然而，
- //有时我们希望能够使用实际的PushButtons（或瞬时开关）物理控制设备) 触发动作，例如打开电灯或风扇，或打开车库门。此外，我们希望 
-//  HomeKit 反映此类手动操作导致的设备变化 - HomeKit 应该知道灯何时打开或手动关闭。
-//一种方法是通过添加到派生服务的 loop() 方法中的自定义代码来监视按钮，检查按钮何时被按下，消除按钮噪音，在按下时执行一些操作，
-//  并使用 setVal( ） 方法。或者您可以使用 HomeSpan 的内置 SpanButton() 对象。
-//SpanButton() 是一个服务级对象，这意味着它将自己附加到您定义的最后一个服务。通常，您将直接在派生服务的构造函数中实例化多个 
- //            SpanButton() 对象。
-//SpanButton() 支持三种类型的触发器：单次按钮按下、双次按下和长（扩展）按下。
-//触发这些不同类型所需的按下的长度可以由 SpanButton() 的可选参数指定。由于大多数按钮在按下时会产生虚假噪音（然后在释放时再次产
-  //           生），触发 SINGLE 按下的默认时间是 5ms。可以将其更改为更长的值，但不建议使用更短的值，因为这可能会允许虚假触发，
- //            除非您使用硬件对开关进行去抖动。
-//SpanButton() 构造函数接受 4 个参数，顺序如下：pin - PushButton 连接到的 pin 号（必需）longTime - 按钮需要按下的最小时间长度
- //            （以毫秒为单位）才能被视为 LONG press (optional; default=2000 ms)singleTime- 最小时间长度（以毫秒为单位） 需要
-  //           按下按钮才能被视为 SINGLE press（可选；默认值=5 ms）doubleTime- 最大时间长度（以毫秒为单位） ) 在两次按下按钮之
-  //间创建两次按下（可选；默认值 = 200 毫秒）
-//当 SpanButton() 被实例化时，它将 ESP32 上的指定引脚设置为带 PULL-UP 的 INPUT，这意味着该引脚在读取时通常会返回 HIGH 值。应连
- // 接实际的 PushButton，以便该引脚接地按下按钮时。
-//HomeSpan 自动轮询所有具有关联 SpanButton() 对象的引脚并检查 LOW 值，这表明按钮被按下，但尚未释放。然后它启动一个计时器。如果
-//  按钮在被按下少于 singleTime 毫秒后被释放，则没有任何反应.如果按钮被按下超过 singleTime 毫秒后释放，但小于 longTime 毫秒，
-//  则触发 SINGLE 按下，除非您在 doubleTime 毫秒内再次按下以触发 DOUBLE 按下。如果按住按钮超过 longTime毫秒而不被释放，触发长按
- //。一旦触发长按，计时器就会重置，因此如果您按住按钮，另一个长按将在另一个 longTime 毫秒内触发。这将一直持续到您最终释放按钮。
+  // In Example 14 we saw how to emulate a PushButton tile within HomeKit by automatically resetting a Characteristic so that
+  // it "turns off" after a short period of time.  However, sometimes we want to be able to physically control a device with actual
+  // PushButtons (or momentary switches) that trigger an action, such as turning on a light or fan, or opening a garage door.
+  // Additionally, we want HomeKit to reflect any changes in the device as a result of such manual actions - HomeKit should know
+  // when the light has been turned on or off manually.
+
+  // One way to accomplish would be via custom code added to the loop() method of your derived Service that monitors a pushbutton,
+  // checks when it is pressed, debounces button noise, performs some actions when pressed, and informs HomeKit of the actions with
+  // the setVal() method. Or you can simply use HomeSpan's built-in SpanButton() object.
+
+  // SpanButton() is a Service-level object, meaning it attaches itself to the last Service you define.  Typically you would instantiate
+  // one of more SpanButton() objects directly inside the constructor for your derived Service.
   
-//请注意，如果您设置 longTime > singleTime，SpanButton() 将仅触发 LONG 按下。此外，如果您将 doubleTime 设置为零，SpanButton() 
-//  将无法触发 DOUBLE 按下。
-//要在派生服务中使用 SpanButton()，您需要实现 button() 方法。与 loop() 方法类似，您的 button() 方法通常包含 getVal() 函数和 
-//  setVal() 函数的某种组合，以及在物理设备上执行一些操作的代码（将引脚设置为高或低，打开风扇等）。但是，与 HomeSpan 每个轮询周
- // 期调用的 loop() 方法相比，HomeSpan 只调用 button()当附加到服务的按钮注册为单次、双次或长按时的方法。
- //同样与循环方法相比，button() 方法采用两个“int”参数，应定义如下：void button(int pin, int pressType)其中“pin”是被触发的
- // PushButton 的 pin 号，并且 pressType 设置为 0 表示单次按下，1 表示双击，2 表示长按。您还可以使用预定义的常量 SpanButton::
-  //SINGLE、SpanButton::DOUBLE 和 SpanButton::LONG 代替数字 0、1 和 2（这是推荐的，尽管您将在示例 16 中看到为什么这些整数不能
-  //被 C++ 枚举类替换）。
-//当然你可以用你自己的名字替换变量“pin”和“pressType”。唯一的要求是定义符合“void button(int, int)”签名。当HomeSpan第一次启动时，
- // 它会检查所有包含一个或更多 SpanButton() 实例以确保您已经实现了自己的 button(int, int) 方法。如果没有，HomeSpan 将在串行监视
-//  器上打印一条警告消息。如果您实例化 SpanButton() 但忘记创建按钮（ ) 方法，或者您使用错误的参数创建它。但也没有什么好事 - 按钮
-//  按下被忽略。C++ 注意：为了额外检查，您还可以在方法定义之后放置上下文关键字“override”：void button(int buttonPin, int pressType) 
-//  override {.. .your code...}这样做可以让编译器检查您是否确实覆盖了基类 button() 方法，而不是无意中创建了一个带有错误签名的
-//  newbutton() 方法，该方法永远不会被 SpanButton() 调用.事实上，您也可以在 update() 和 loop() 方法的定义中添加“覆盖”，因为它们
- // 总是应该覆盖基类方法。
-//为了演示 SpanButtons 在实践中的工作原理，我们将从示例 11 中使用的相同 LED 代码开始实现一个可调光 LED，但使用 3 个 SpanButton() 
-//  对象执行不同的功能，展示不同类型的按下。将响应单次按下切换电源，打开电源并将亮度设置为“最喜欢”级别以响应双击，并设置一个新的
-//  “最喜欢”级别以响应长按。* A“提升亮度” SpanButton，它将在单次按下时增加 1% 的亮度，在长按时重复增加 10% 的亮度，并在双次按下时
- // 跳到最大亮度。*“较低的亮度" SpanButton 响应单次按下会降低 1% 的亮度，响应长按会重复降低 10% 的亮度，并会跳到最小亮度以响应双击。
-//像往常一样，所有代码都在 DEV_LED.h 中实现，带有 NEW！注释突出了示例 11 中的更改。您还会注意到，我们已经扩展了此版本派生的可调光 
-//  LED 服务的构造函数，以包括每个按钮的引脚号。有关详细信息，请参阅 DEV_LED.h。
+  // SpanButton() supports three types of a triggers: a SINGLE button press, a DOUBLE press, and a LONG (extended) press.
+  
+  // The length of the presses needed to trigger these different types can be specified by optional arguments to SpanButton().
+  // Since most buttons create spurious noise when pressed (and then again when released), the default time to trigger a SINGLE press is 5ms.
+  // It's fine to change this to a longer value, but a shorter value is not recommended as this may allow spurious triggers unless
+  // you debounce your switch with hardware.
+  
+  // The SpanButton() constructor takes 5 arguments, in the following order:
+  //
+  //  pin         - the pin number to which the PushButton is attached (required)
+  //  longTime    - the minimum length of time (in milliseconds) the button needs to be pushed to be considered a LONG press (optional; default=2000 ms)
+  //  singleTime  - the minimum length of time (in milliseconds) the button needs to be pushed to be considered a SINGLE press (optional; default=5 ms)
+  //  doubleTime  - the maximum length of time (in milliseconds) between button presses to create a DOUBLE press (optional; default=200 ms)
+  //  triggerType - the action that causes a trigger on the pin (optional; default=SpanButton::TRIGGER_ON_LOW).  Built-in choices include:
+  //
+  //      SpanButton::TRIGGER_ON_LOW:   used for a button that connects pin to GROUND
+  //      SpanButton::TRIGGER_ON_HIGH:  used for a button that connects pin to VCC (typically +3.3V)
+  //      SpanButton::TRIGGER_ON_TOUCH: used when a pin is connected to a touch pad/sensor
+
+  // When a SpanButton() is first instantiated, HomeSpan configures the specified pin in accordance with the triggerType chosen.
+
+  // Then, HomeSpan continuously polls all pins with associated SpanButton() objects and checks for triggers, which indicates the button was
+  // pressed, but not yet released.  It then starts a timer.  If the button is released after being pressed for less than singleTime milliseconds,
+  // nothing happens.  If the button is released after being pressed for more than singleTime milliseconds, but for less than longTime milliseconds,
+  // a SINGLE press is triggered, unless you press once again within doubleTime milliseconds to trigger a DOUBLE press.  If the button is held for more
+  // than longTime milliseconds without being released, a LONG press is triggered. Once a LONG press is triggered the timer resets so that if you keep
+  // holding the button, another LONG press will be triggered in another longTime milliseconds.  This continues until you finally release the button.    
+
+  // Note if you set longTime > singleTime, SpanButton() will only trigger LONG presses.  Also, if you set doubleTime to zero, SpanButton() will not be
+  // able to trigger a DOUBLE press.
+
+  // To use SpanButton() within a derived Service you need to implement a button() method.  Similar to the loop() method, your button()
+  // method will typically contain some combination of getVal() functions and setVal() functions, along with code that performs some set
+  // of actions on the physical device (seting pins high or low, turning on fans, etc).  However, in contrast to the loop() method, which
+  // is called by HomeSpan every polling cycle, HomeSpan only calls the button() method when a button attached to the Service registers a
+  // SINGLE, DOUBLE, or LONG press.
+
+  // Also in contrast with the loop method, the button() method takes two 'int' arguments, and should defined as follows:
+  //
+  //    void button(int pin, int pressType)
+  //
+  // where "pin" is the pin number of the PushButton that was triggered, and pressType is set to 0 for a SINGLE press, 1 for a DOUBLE press,
+  // and 2 for a LONG press.  You can also use the pre-defined constants SpanButton::SINGLE, SpanButton::DOUBLE, and SpanButton::LONG in place
+  // of the numbers 0, 1, and 2 (this is recommended, though you will see in Example 16 why these integers can't be replaced by an C++ enum class).
+  
+  // Of course you can replace the variables "pin" and "pressType" with your own names.  The only requirement is the definition conform to
+  // the "void button(int, int)" signature.  When HomeSpan first starts up it checks all Services containing one or more SpanButton() instances to
+  // ensure you've implemented your own button(int, int) method.  If not, HomeSpan will print a warning message on the Serial Monitor.  Nothing bad
+  // happens if you instantiate a SpanButton() but forget to create the button() method, or you create it with the wrong parameters. But nothing good
+  // happens either - button presses are just ignored.
+  //
+  // C++ Note:  For an extra check, you can also place the the contextual keyword "override" after your method definition as such:
+  //
+  //    void button(int buttonPin, int pressType) override {...your code...}
+  //
+  // Doing so allows the compiler to check that you are indeed over-riding the base class button() method and not inadvertently creating a new
+  // button() method with an incorrect signature that will never be called by SpanButton().  In fact, you could add "override" to the definition
+  // of your update() and loop() methods as well, since these are always supposed to over-ride the base-class method.
+
+  // To demonstrate how SpanButtons works in practice, we will implement a Dimmable LED starting with the same LED code use in Example 11,
+  // but with 3 SpanButton() objects performing different functions that showcase the different types of presses.
+  //
+  //  * A "power" SpanButton that will toggle the power in response a SINGLE press, turn on the power and set the brightness to a "favorite" level
+  //    in response to the DOUBLE press, and set a new "favorite" level in response to a LONG press.
+  //
+  //  * A "raise brightness" SpanButton that will increase the brightness by 1% in response to a SINGLE press, repeatedly increase the brightness
+  //    by 10% in response to a LONG press, and jump to the maximum brightness in response to a DOUBLE press.
+  //
+  //  * A "lower brightness" SpanButton that will decrease the brightness by 1% in response to a SINGLE press, repeatedly decrease the brightness
+  //    by 10% in response to a LONG press, and jump to the minimum brightness in response to a DOUBLE press.
+  
+  // As usual, all the code is implemented in DEV_LED.h, with NEW! comments highlighting changes from Example 11.  You'll also notice that we've
+  // extended the constructor for this version of our derived Dimmable LED Service to include the pin numbers for each of our buttons.
+  // See DEV_LED.h for details.
   
   Serial.begin(115200);
 
@@ -99,7 +142,7 @@ void setup() {
       new Characteristic::Identify(); 
       new Characteristic::Name("PushButton LED");
     
-    new DEV_DimmableLED(17,23,5,18);          // 新的！ 添加了三个额外的参数来指定三个 SpanButtons() 的引脚号 - 请参阅 DEV_LED.h
+    new DEV_DimmableLED(17,23,5,18);          // NEW! added three extra arguments to specify the pin numbers for three SpanButtons() - see DEV_LED.h
  
 } // end of setup()
 
@@ -110,3 +153,115 @@ void loop(){
   homeSpan.poll();
   
 } // end of loop()
+
+////////////////  ADDITIONAL NOTES ////////////////////////
+  
+  // DEFAULT VALUES AND ALTERNATIVE CONSTRUCTORS
+  // --------------------------------------------
+  
+  // As shown in this example, the following creates a SpanButton suitable for connecting pin 23 to GROUND via a pushbutton, and uses
+  // SpanButton's default values for longTime, singleTime, and doubleTime:
+  //
+  //    new SpanButton(23);
+  //
+  // This is exactly the same as if you explicitly set each parameter to its default value:
+  //
+  //    new SpanButton(23,2000,5,200,SpanButton::TRIGGER_ON_LOW);  // equivalent to above
+  //    
+  // If instead you want to create a SpanButton that connects pin 23 to VCC via a pushbutton using SpanButton::TRIGGER_ON-HIGH,
+  // you need to explictly set all the other parameters, even if you are satisfied with their default values, since triggerType
+  // is the last argument in the constructor:
+  //
+  //    new SpanButton(23,2000,5,200,SpanButton::TRIGGER_ON_HIGH);
+  //
+  // Because this can be cumbersome, SpanButton includes an alternative constructor where triggerType is the second paramater, instead
+  // of the last.  In this case triggerType is required, but longTime, singleTime, and doubleTime are still optional.
+  //
+  // For example, the following creates a SpanButton suitable for connecting pin 23 to a touch pad/sensor, and uses
+  // SpanButton's default values for longTime, singleTime, and doubleTime:  
+  //
+  //    new SpanButton(23,SpanButton::TRIGGER_ON_TOUCH);
+  //
+  // which is of course equivalent to:
+  //
+  //    new SpanButton(23,SpanButton::TRIGGER_ON_TOUCH,2000,5,200);
+
+  
+  // TOUCH PAD/SENSOR CALIBRATION
+  // ----------------------------
+  
+  // SpanButton makes use of the ESP32's internal touch sensor peripheral to monitor pins for "touches".  There are a number
+  // of paramaters that must be specified for touches to be accurately detected, depending on the exact size and shape of your
+  // touch pads.  Upon instantiation of a SpanButton() with triggerType=SpanButton::TRIGGER_ON_TOUCH, SpanButton will conveniently
+  // perform an automatic calibration that sets an appropriate threshold level for detecting touches.
+  //
+  // However, if you need to, you can override this calibration process using the following two class-level functions:
+  //
+  //    SpanButton::setTouchThreshold()    - explicitly sets the threshold for detecting touches (i.e. overrides the auto-calibration)
+  //    SpanButton::setTouchCycles()       - explicitly sets the measurement and sleep times used by the ESP32's internal touch peripheral
+  //
+  // See the SpanButton secion of the Reference API for details on how to use these optional functions.
+
+
+  // THE triggerType FUNCTION
+  // -------------------------
+  
+  // Though the three triggerType objects supported by SpanButton (SpanButton::TRIGGER_ON_LOW, etc.) may appear to be nothing more than
+  // constants, they are actually boolean functions that each accept a single integer argument.  When SpanButton calls the triggerType function,
+  // it passes the pin number specified in the constructor as the integer argument, and the triggerType function returns TRUE if the
+  // "pushbutton" associated with the pin number is "pressed," or FALSE if it is not.
+  //
+  // For example, the definitions of SpanButton::TRIGGER_ON_LOW and SpanButton::TRIGGER_ON_HIGH are as follows:
+  //
+  //    boolean TRIGGER_ON_LOW(int pinArg)  { return( !digitalRead(pinArg) ); }
+  //    boolean TRIGGER_ON_HIGH(int pinArg) { return(  digitalRead(pinArg) ); }
+  //
+  // The definitions for SpanButton::TRIGGER_ON_TOUCH are more complicated since the ESP32 touch sensor library returns either a 2-byte
+  // or 4-byte numeric value when the state of pin configured as a touch sensor is read, rather than a simple 0 or 1.  The triggerType
+  // function must therefore compare the value read from the touch sensor pin to some pre-computed "threshold" to determine whether or not
+  // the touch pad has in fact been touched.  This is the threshold value that HomeSpan auto-calibrates for you as described above.  
+  // 
+  // Making things even more complex is that the ESP32 touch pins work in the reverse direction as touch pins on the ESP32-S2 and ESP32-S3.
+  // On the former, the values read from a touch sensor DECREASE when the touch pad is touched.  On the latter, the values increase when the
+  // touch pad is touched.  This means that for ESP32 devices, HomeSpan uses the following definition for SpanButton::TRIGGER_ON_TOUCH:
+  //
+  //    boolean TRIGGER_ON_TOUCH(int pinArg) { return ( touchRead(pinArg) < threshold ); }
+  //
+  // whereas on ESP32-S2 and ESP32-S3 devices, HomeSpan uses a definition that flips the direction of the comparison:
+  //
+  //    boolean TRIGGER_ON_TOUCH(int pinArg) { return ( touchRead(pinArg) > threshold ); }
+  //
+  // For ESP32-C3 devices, HomeSpan does not define TRIGGER_ON_TOUCH at all since there are no touch pins on an ESP32-C3 device!  The compiler
+  // will throw an error if you try to create a SpanButton with triggerType=SpanButton::TRIGGER_ON_TOUCH, or if you call either of the
+  // calibration functions above.
+  //
+
+  // CREATING YOUR OWN triggerType FUNCTION
+  // --------------------------------------
+
+  // You are not limited to choosing among HomeSpan's three built-in triggerType functions. You can instead create your own triggerType function
+  // and pass it to SpanButton as the triggerType parameter in the SpanButton constructor.  Your function must be of the form `boolean func(int)`,
+  // and should return TRUE if the "pushbutton" associated with the pin number that HomeSpan passes to your function as the integer argument
+  // has been "pressed", or FALSE if it has not.  This allows you to expand the used of SpanButton to work with pin multiplexers, pin extenders,
+  // or any device that may require custom handling via a third-party library.
+  //
+  // For example, if you were using an MCP I/O Port Expander with the Adafruit mcp library, you could create a triggerType function for a pin
+  // on the MCP device that is connected to ground through a pushbutton as such:
+  //
+  //    boolean MCP_READ(int mcpPin) { return ( !mcp.digitalRead(mcpPin); ) }
+  //
+  // And then simply pass MCP_READ to SpanButton as the triggerType parameter using any of the SpanButton constuctors:
+  //
+  //    new SpanButton(23,MCP_READ);                      // uses default longTime, singleTime, and doubleTime
+  //    new SpanButton(23,MCP_READ,2000,5,200);           // expliclty sets longTime, singleTime, and doubletime
+  //    new SpanButton(23,2000,5,200,MCP_READ);           // alternative constructor with arguments in a different order
+  //
+  // Alternatively, you can use a lambda function as the triggerType parameter, thus creating your function on the fly when instantiating a SpanButton:
+  //
+  //    new SpanButton(23,[](int mcpPin)->boolean{ return ( !mcp.digitalRead(mcpPin); ) }
+  //
+  // Note: If you create your own triggerType function, don't forget to perform any initialization of the "pin", or setup/configuration of a
+  // pin extender, etc., prior to instantiating a SpanButton that uses your custom function.  HomeSpan cannot do this for you.
+  //
+  
+  
