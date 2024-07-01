667a48b219ca17186732a5c9b874cadecc574498
abackup <40161397+abackup@users.noreply.github.com>
2024-07-01 13:29:16 +0800

    Update DEV_Blinker.h

--- examples/14-EmulatedPushButtons/DEV_Blinker.h
+++ examples/14-EmulatedPushButtons/DEV_Blinker.h
@@ -1,81 +1,75 @@
-
 ////////////////////////////////////
-//   DEVICE-SPECIFIC LED SERVICES //
+//       设备专用 LED 服务         //
 ////////////////////////////////////
 
-// 注意：此示例仅用于演示如何在 HomeSpan 中模拟按钮。 闪烁例程的长度比 HomeSpan 应该花费在 update() 
-//上的时间长得多。 要查看这对 HomeKit 的影响，请尝试将闪烁次数更改为 50，或将其保持为 3 并增加 update() 
-//  中的延迟时间，以使闪烁例程花费 10 秒或更长时间。 激活后，HomeKit 如果在一定时间内没有收到 update() 的
- // 返回消息，就会认为设备已经无响应。
+// 注意：此示例仅用于演示如何在 HomeSpan 中模拟按钮。闪烁例程的长度比 HomeSpan 在 update() 上花费的时间长得多。
+// 要查看这对 HomeKit 的影响，请尝试将闪烁次数更改为 50，或将其保持在 3 次并增加 update() 中的延迟时间，
+// 以便闪烁例程需要 10 秒或更长时间。激活后，如果设备在一定时间内未收到来自 update() 的返回消息，HomeKit 会认为设备已无响应。
 
-// 在实践中，按钮仿真用于非常短的例程，例如驾驶
-// 一个 IR LED 或一个 RF 发射器，用于向远程设备发送代码。
+// 实际上，按钮模拟用于非常短的例程，例如驱动红外 LED 或 RF 发射器将代码发送到远程设备。
 
-// 与示例 9 相比的新行和更改行被标记为“新！”
+// 与示例 9 相比，新增和更改的行标记为“新！”
 
-struct DEV_Blinker : Service::LightBulb {           // LED Blinker
+struct DEV_Blinker : Service::LightBulb {           // LED 闪光灯
 
-  int ledPin;                                       // pin number defined for this LED
-  int nBlinks;                                      // NEW! number of times to blink
+  int ledPin;                                       // 为此 LED 定义的引脚编号
+  int nBlinks;                                      // 新功能！闪烁次数
   
-  SpanCharacteristic *power;                        // reference to the On Characteristic
+  SpanCharacteristic *power;                        // 引用 On 特性
   
-  DEV_Blinker(int ledPin, int nBlinks) : Service::LightBulb(){       // constructor() method
+  DEV_Blinker(int ledPin, int nBlinks) : Service::LightBulb(){       // 构造函数（）方法
 
     power=new Characteristic::On();                 
         
     this->ledPin=ledPin;                            
-    this->nBlinks=nBlinks;                           // NEW! number of blinks
+    this->nBlinks=nBlinks;                          // 新功能！闪烁次数
     pinMode(ledPin,OUTPUT);                         
     
-    Serial.print("Configuring LED Blinker: Pin=");   // initialization message
+    Serial.print("Configuring LED Blinker: Pin=");   // 初始化消息
     Serial.print(ledPin);
-    Serial.print("  Blinks=");                       // NEW! add output message for number of blinks
+    Serial.print("  Blinks=");                       // 新功能！添加闪烁次数的输出消息
     Serial.print(nBlinks);
     Serial.print("\n");
 
-  } // end constructor
+  } // 结束构造函数
 
-  boolean update(){                              // update() method
+  boolean update(){                              // update() 方法
 
-    // 新的！ 我们不是根据 newValue 打开或关闭 LED，而是将其闪烁
-     // 指定的次数，完成后保持在关闭位置。
-     // 此行已删除...
+    // 新功能！我们不再根据 newValue 打开或关闭 LED，而是按照指定的次数闪烁，并在完成后将其保持在关闭位置。此行已删除...
     
     // digitalWrite(ledPin,power->getNewVal());      
 
-    // 并被如下取代
+    // 并被替换为...
 
-    if(power->getNewVal()){                       // 检查以确保 HomeKit 要求我们“打开”这个设备（否则忽略）
+    if(power->getNewVal()){                       // 检查以确保 HomeKit 请求我们“打开”此设备（否则忽略）
 
       LOG1("Activating the LED Blinker on pin=");
       LOG1(ledPin);
       LOG1("\n");
 
-      for(int i=0;i<nBlinks;i++){                     // 循环指定的闪烁次数
-        digitalWrite(ledPin,HIGH);                    // turn pin on
-        delay(100);                                   // wait 100 ms
-        digitalWrite(ledPin,LOW);                     // turn pin off
-        delay(250);                                   // wait 250 ms
+      for(int i=0;i<nBlinks;i++){                     // 循环指定闪烁次数
+        digitalWrite(ledPin,HIGH);                    // 打开引脚
+        delay(100);                                   // 等待 100 毫秒
+        digitalWrite(ledPin,LOW);                     // 关闭引脚
+        delay(250);                                   // 等待 250 毫秒
       }
       
-    } // if newVal=true
+    } // 如果 newVal=true
 
-    // 请注意，以上 100 毫秒和 250 毫秒的延迟仅用于说明目的
-     //（所以你可以看到 LED 闪烁）。 在实践中，如果您控制的是 IR LED
-     // 或射频发射器，整个信号可能会在 10 毫秒内传输。
-    return(true);                               // return true
+    // 请注意，上述 100 毫秒和 250 毫秒的延迟仅用于说明目的（因此您可以看到 LED 闪烁）。
+   // 实际上，如果您控制的是红外 LED 或 RF 发射器，则整个信号可能总共需要 10 毫秒才能传输完毕。
+    
+    return(true);                               // 返回 true
   
-  } // update
+  } //  更新
 
- //新的！ 这里我们实现了一个非常简单的 loop() 方法，它检查电源特性是否
-   //“开启”至少 3 秒。 如果是这样，它会将值重置为“关闭”（假）。
+  // 新功能！我们在这里实现一个非常简单的 loop() 方法，该方法检查电源特性是否至少处于“开启”状态 3 秒。如果是，则将值重置为“关闭”（false）。
 
   void loop(){
 
-    if(power->getVal() && power->timeVal()>3000){   //检查 power 是否为真，并且自上次修改以来的时间大于 3 秒
-      LOG1("Resetting Blinking LED Control\n");     // log message  
-      power->setVal(false);                         // set power to false
+    if(power->getVal() && power->timeVal()>3000){   // 检查 power 是否正确，以及自上次修改以来的时间是否大于 3 秒
+      LOG1("Resetting Blinking LED Control\n");     // 记录消息
+      power->setVal(false);                         // 将 power 设置为 false
     }      
     
   } // loop
@@ -84,9 +78,9 @@ struct DEV_Blinker : Service::LightBulb {           // LED Blinker
       
 //////////////////////////////////
 
-// HomeKit 错误说明：在本示例的开发过程中发现了 HomeKit 中的一个明显错误。
-// 如果你有一个Accessory 有三个或更多Service，并且Accessory 收到通知消息
-// 来自设备，并且 HomeKit 界面打开以显示此 Service tile 的详细控制
-// 在 HomeKit 应用程序中，由于某种原因 HomeKit 会向设备发送一个 update() 请求，请求
-// 将 Characteristic 设置为它刚刚从事件通知接收到的值。 不应该使用 HomeKit
-// 发送更新请求以响应事件通知。
+// HomeKit 错误说明：在开发此示例期间，发现了 HomeKit 中存在一个明显的错误。
+
+// 如果您有一个具有三个或更多服务的配件，并且该配件从设备收到通知消息，并且 HomeKit 界面已打开以在 HomeKit 应用中显示此
+// 服务图块的详细控件，那么出于某种原因，HomeKit 会将 update() 请求发送回设备，要求将特性设置为刚从事件通知中收到的值。
+
+// HomeKit 不应该发送更新请求来响应事件通知。
